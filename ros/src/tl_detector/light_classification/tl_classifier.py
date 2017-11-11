acacc95d09ac687e99d26829c1ba0eb88682d379
sachitv <sachit722448>
2017-11-10 20:35:34 -0800

    Fixed changes to bad file

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -13,7 +13,6 @@ import scipy.misc
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
         PATH_TO_MODEL = 'frozen_models/frozen_inference_graph.pb'
         PATH_TO_LABELS = 'label_map_3.pbtxt'
         NUM_CLASSES = 3
@@ -28,7 +27,6 @@ class TLClassifier(object):
         self.detection_graph = tf.Graph()
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
-            # Works up to here.
             with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
@@ -39,7 +37,6 @@ class TLClassifier(object):
             self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
             self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
         self.sess = tf.Session(graph=self.detection_graph)
-        self.counter = 1
         pass
 
     def get_classification(self, image):
@@ -52,7 +49,6 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
         rospy.loginfo('trying to classify image')
         with self.detection_graph.as_default():
             # Expand dimension since the model expects image to have shape [1, None, None, 3].
@@ -60,17 +56,14 @@ class TLClassifier(object):
             img_expanded = np.expand_dims(image, axis=0)  
             boxes, scores, classes, num = self.sess.run( [self.d_boxes, self.d_scores, self.d_classes, self.num_d], feed_dict={self.image_tensor: img_expanded})
             outimage = image
-            imagename = '/home/sachit/Desktop/read images/image' + str(self.counter).zfill(5) + '.jpg'
-            self.counter+=1
-            scipy.misc.imsave(imagename, outimage)
             vis_util.visualize_boxes_and_labels_on_image_array(outimage, np.squeeze(boxes), np.squeeze(classes).astype(np.int32), np.squeeze(scores), self.category_index, use_normalized_coordinates=True, line_thickness=8)
 
             try:
-                #self.image_pub.publish(self.bridge.cv2_to_imgmsg(outimage, "bgr8"))
                 self.image_pub.publish(self.bridge.cv2_to_imgmsg(outimage, "rgb8"))
             except CvBridgeError as e:
                 print(e)
 
+            #Basically get the color with the highest score
             color = 4
             if(num > 0):
                 if(scores[0][0] > 0.6):
